40a352f69f578c0c281ee052bf85313d082248d0
EmilioSzH <sanchezharris.e@northeastern.edu>
2025-04-13 15:04:18 -0500

    edge case testing and sort/filter for houses

--- src/main/java/maintainhome/model/UnitFilters.java
+++ src/main/java/maintainhome/model/UnitFilters.java
@@ -4,7 +4,8 @@ package maintainhome.model;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
-import maintainhome.model.Home.Types.RoomType;
+
+import maintainhome.model.Home.Home;
 import maintainhome.model.Home.UnitItems.IUnit;
 
 public class UnitFilters {
@@ -24,5 +25,11 @@ public class UnitFilters {
         return units.stream()
                 .filter(u -> u.getInstallDate().isAfter(date))
                 .collect(Collectors.toList());
+    }
+    
+    public static List<Home> filterByHomeName(List<Home> homes, String homeName) {
+    return homes.stream()
+            .filter(home -> home.getHomeName().equalsIgnoreCase(homeName))
+            .collect(Collectors.toList());
     }
 }
--- src/main/java/maintainhome/model/UnitSorters.java
+++ src/main/java/maintainhome/model/UnitSorters.java
@@ -2,11 +2,14 @@
 package maintainhome.model;
 
 import java.util.Comparator;
+
+import maintainhome.model.Home.Home;
 import maintainhome.model.Home.UnitItems.IUnit;
 
 public class UnitSorters {
     public static final Comparator<IUnit> BY_TYPE = Comparator.comparing(IUnit::getUnitType);
     public static final Comparator<IUnit> BY_ROOM = Comparator.comparing(IUnit::getRoomType);
     public static final Comparator<IUnit> BY_INSTALL_DATE = Comparator.comparing(IUnit::getInstallDate);
+    public static final Comparator<Home> BY_HOME_NUM = Comparator.comparingInt(Home::getHomeNum);
 }
 
