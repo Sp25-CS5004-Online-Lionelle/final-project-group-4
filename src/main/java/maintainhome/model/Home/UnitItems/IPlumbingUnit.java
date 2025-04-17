3429f7ff9d4874d90d44b49f92c642fad31ff369
lkim016 <kim.lori57@gmail.com>
2025-04-16 23:53:44 -0700

    added comment - last commenct are model, filters, sorters

--- src/main/java/maintainhome/model/Home/UnitItems/IPlumbingUnit.java
+++ src/main/java/maintainhome/model/Home/UnitItems/IPlumbingUnit.java
@@ -4,5 +4,10 @@ package maintainhome.model.Home.UnitItems;
  * The interface for the Plumbing Unit.
  */
 public interface IPlumbingUnit {
+    /**
+     * Gets the Plumbing Unit's measure of gallons.
+     * 
+     * @return the plumbing unit's measure of gallons
+     */
     int getPlumbingGallon();
 }
